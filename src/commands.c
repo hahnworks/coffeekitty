4c158084a2c81d9b42165277c87ed17318702ad8
hahnworks <200227747+hahnworks@users.noreply.github.com>
2025-04-24 17:45:22 +0200

    Fix copyright notice

--- src/commands.c
+++ src/commands.c
@@ -1,7 +1,7 @@
 /*
  * This file is part of Coffeekitty
  * 
- * Copyright (C) 2025 Alexander Hahn Hahn
+ * Copyright (C) 2025 Alexander Hahn
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the European Union Public License (EUPL), version 1.2.
